db0433e4774c92be9489e3368d1a072765bdc15a
maharjan <rohit@kodiary.com>
2025-07-07 13:34:44 +0545

    update

---
+++ .example.js
@@ -0,0 +1,15 @@
+NEXT_PUBLIC_BASE_URL="http://localhost:3000"
+
+DATABASE_URL="postgresql://postgres:3624@localhost:5432/bookaroo"
+NEXTAUTH_URL=http://localhost:3000
+NEXTAUTH_SECRET=93bb9bb75b835acf9496908ce4c23ca16d12bb8dac39e324453487176435d160
+API_KEY=5596e865a7cb3dacf8e76abf15109a449aec8cd5daecd1b58323401e2627af07
+
+UPSTASH_REDIS_REST_URL="https://top-haddock-44166.upstash.io"
+UPSTASH_REDIS_REST_TOKEN="AayGAAIjcDExMWVlNzEzNTllYTg0Y2VmODI0NzhhNDgzZDUwODBjMXAxMA"
+
+SMTP_HOST=smtp.gmail.com
+SMTP_PORT=587
+SMTP_USER=swift.rohit13@gmail.com
+SMTP_PASS=omhhcokcijttnwyk
+NEXTAUTH_URL=http://localhost:3000
