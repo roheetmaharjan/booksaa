b1e182f1e1a3546e087daf4be3a2facc184e5926
Maharjan <rohit@kodiary.com>
2025-07-08 18:01:41 +0545

    vendors

--- app/api/vendors/create/route.js
+++ app/api/vendors/create/route.js
@@ -1,3 +1,24 @@
 import { NextResponse } from "next/server";
 
-import { PrismaClient } from "@prisma/client";
+export async function POST(request) {
+    try{
+        const body = await request.json();
+        const {name,location,description,category} = body
+        if(!name || !location || !description || !category){
+            return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
+        }
+        const newVendor = await prisma.vendor.create({
+            data: {
+                name,
+                location,
+                description,
+                category,
+            },
+        });
+        return NextResponse.json({newVendor}, { status: 201 });
+    }
+    catch(error){
+        console.error(error);
+        return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+    }
+}
