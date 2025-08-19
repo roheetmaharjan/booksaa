0fd564a77e3c066b5843ea09b191c33d46264d68
Maharjan <rohit@kodiary.com>
2025-08-19 16:20:10 +0545

    service api addedd

--- app/api/services/create/route.js
+++ app/api/services/create/route.js
@@ -1,17 +1,31 @@
-// app/api/categories/create/route.js
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function POST(req) {
   try {
     const { name, price, duration, description } = await req.json();
-
-    if (!name || !price || !duration) {
+    if (!vendorId) {
       return NextResponse.json(
-        { error: "Name, price, duration are required" },
+        { error: "Vendor ID is required" },
         { status: 400 }
       );
     }
+    
+    if (!name?.trim() || isNaN(price) || isNaN(duration)) {
+      return NextResponse.json(
+        { error: "Invalid input: name, price, duration are required" },
+        { status: 400 }
+      );
+    }
+
+    const vendorExists = await prisma.vendor.findUnique({
+      where: { id: vendorId },
+    });
+
+    if (!vendorExists) {
+      return NextResponse.json({ error: "Vendor not found" }, { status: 404 });
+    }
+
 
     const exists = await prisma.service.findUnique({
       where: { name },
@@ -25,7 +39,7 @@ export async function POST(req) {
     }
 
     const service = await prisma.service.create({
-      data: { name, description, price, duration },
+      data: { name, description, price, duration,vendorId },
     });
 
     return NextResponse.json(service, { status: 201 });
